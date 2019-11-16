85d32fa47a0be6bf0b44e6982d832e24f7b81db9
heptaluan <vd2o@foxmail.com>
2019-11-16 23:29:57 +0800

    add redux example

---
+++ redux-example/src/context/ThemeSwitch.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class ThemeSwitch extends Component {
+  render() {
+    return (
+      <div>
+        <button>red</button>
+        <button>blue</button>
+      </div>
+    )
+  }
+}
